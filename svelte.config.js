566d81f86c5baf73652e5cb704e50eaf0392f93c
Ramesh <klarixaramesh@gmail.com>
2026-01-18 12:25:43 +0800

    Add favicon and configure prerender error handling

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			handleHttpError: ({ path, referrer, message }) => {
+				// ignore deliberate link to missing favicon
+				if (path === '/favicon.png') return;
+
+				// otherwise fail the build
+				throw new Error(message);
+			}
+		}
 	},
 	preprocess: vitePreprocess()
 };
